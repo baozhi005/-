af39accf68ce49e4f78d9fb7494db2037da67780
Ximing WANG <sighwang@outlook.com>
2019-06-01 20:54:18 +0800

    format

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ var cartItems = [
 ]
 
 var discountProductList = [
-    {name: '可口可乐', count: 1, unit: '瓶'},
-    {name: '羽毛球', count: 1, unit: '个'},
-    
+    { name: '可口可乐', count: 1, unit: '瓶' },
+    { name: '羽毛球', count: 1, unit: '个' },
+
 ]
 
 var summary = {
@@ -43,24 +43,24 @@ var result = {
 }
 function generatorTemplate(cartItems) {
     var template = ''
-    cartItems.forEach(function(cartItem) {
-        template += '<tr>' + 
-        '<td><img src="' + cartItem.img + '"></td>' +         
-        '<td>' + cartItem.name + '</td>' + 
-        '<td>' + cartItem.count + cartItem.unit+ '</td>' + 
-        '<td>' + cartItem.price.toFixed(2) + '(元)</td>' + 
-        '<td>' + cartItem.totalPrice.toFixed(2)+ '(元)</td></tr>';
+    cartItems.forEach(function (cartItem) {
+        template += '<tr>' +
+            '<td><img src="' + cartItem.img + '"></td>' +
+            '<td>' + cartItem.name + '</td>' +
+            '<td>' + cartItem.count + cartItem.unit + '</td>' +
+            '<td>' + cartItem.price.toFixed(2) + '(元)</td>' +
+            '<td>' + cartItem.totalPrice.toFixed(2) + '(元)</td></tr>';
     })
     return template
 }
 
 function generatorDiscountProductListTemplate(discountProductList) {
     var template = ''
-    discountProductList.forEach(function(product) {
+    discountProductList.forEach(function (product) {
         template += '<li class="discount-product-item">' +
-         '<div class="item"><span class="name">名称：</span><span class="value">' + product.name + '</span></div>' +
-         '<div class="item"><span class="name">数量：</span><span class="value">' + product.count + product.unit + '</span></div>' +
-         '</li>'
+            '<div class="item"><span class="name">名称：</span><span class="value">' + product.name + '</span></div>' +
+            '<div class="item"><span class="name">数量：</span><span class="value">' + product.count + product.unit + '</span></div>' +
+            '</li>'
     })
     return template
 }
@@ -81,9 +81,13 @@ function setDiscountPrice(discountPrice) {
     document.querySelector('.discount-price').textContent = discountPrice.toFixed(2) + '(元)'
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function renderView(result) {
     renderTable(result.cartItems)
     renderDiscountProductList(result.discountProductList)
     setTotlePrice(result.summary.totalPrice)
     setDiscountPrice(result.summary.discountPrice)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderView(result)
 })
